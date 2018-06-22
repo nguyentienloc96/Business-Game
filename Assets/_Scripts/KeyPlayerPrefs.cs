3a06539329348d6accf9ba23943fae943e093468
KhanhDat <keepyoursmile.512@gmail.com>
2018-06-22 09:37:31 +0700

    new project and somthings

---
+++ Assets/_Scripts/KeyPlayerPrefs.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPlayerPrefs : MonoBehaviour
+{
+    public static string dollar = "Dollar"; //Tiền $ trong game
+
+    public static string btc = "Btc"; //Tiền Bitcoin
+}

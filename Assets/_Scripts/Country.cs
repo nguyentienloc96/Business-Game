be0bdf5e4ec261f14e451efc73b57b956258843c
LOCNT-TT-D3\nguye <nguyenloc271096@gmail.com>
2018-06-25 00:23:48 +0700

    Test

--- Assets/_Scripts/Country.cs
+++ Assets/_Scripts/Country.cs
@@ -1,18 +1,117 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class Country : MonoBehaviour {
+public struct BASIC
+{
+    public int postage;
+    public DateTime startDate;
+    public DateTime dayOfInvestment;
+}
+
+public struct PROCEDUREPROCESS
+{
+    public BASIC rdb;   //R&D
+    public BASIC f;     //Factory/workshop
+    public BASIC o;     //Outsource
+    public BASIC b;     //Buying other factory/workshop
+    public BASIC s;     //Service
+
+    public void SetRANDD(float SP0) // Setup it with the day
+    {
+        if (rdb.postage > 10000)
+        {
+            if (UnityEngine.Random.Range(0f, 1f) <= 0.2f * GameManager.Instance.SRD)
+            {
+                SP0 += 1000000;
+            }
+        }
+    }
+
+    public void SetFactoryWorkshop(float SP0) // Setup it with the Month
+    {
+        if (f.postage > 100000 && (long)((TimeSpan)(GameManager.Instance.dateGame - f.startDate)).TotalDays >= 365)
+        {
+            if ((long)((TimeSpan)(GameManager.Instance.dateGame - f.dayOfInvestment)).TotalDays >= 30)
+            {
+                SP0 += (f.postage - 100000) * 0.08f;
+                f.postage -= 50;
+                f.dayOfInvestment = GameManager.Instance.dateGame;
+            }
+        }
+    }
+
+    public void Outsource(float SP0)
+    {
+        SP0 += o.postage * 0.7f;
+    }
+
+    public void BuyingSotherFactoryWorkshop(float SP0)
+    {
+        if (f.postage > 200000)
+        {
+            SP0 += (b.postage - 100000) * 0.05f;
+        }
+    }
+
+}
+
+public struct ADS
+{
+    public float t;     //Traditional ads
+    public float s;     //Social network ads
+    public float w;     //Website & affiliate
+}
+
+public struct SPREADING
+{
+    public float le;    //Local Economy research
+}
+
+public struct TRANSPORTCHAIN
+{
+    public float tax;   //Transport agent
+    public float w;     //Warehouse Storage
+    public float vs;    //Vehicles: self transport
+}
+
+public struct SALESCHAIN
+{
+    public float b;     //Build a shop
+    public float a;     //Link with agencies shop
+    public float o;     //Online shop
+    public float sc;    //Sales culture
+}
+
+public struct RISKMANAGEMENT
+{
+    public float m;     //Media crisis
+    public float l;     //Law crisis
+    public float e;     //Employee crisis
+}
+
+public struct EMPLOYEES
+{
+    public float st1;   //Self training 1
+    public float st2;   //Self training 2
+    public float hr;    //Human resource
+    public float ph;    //People hiring
+    public float cc;    //Company culture i : 15
+    public float ae;    //Annually event i : 20
+    public float ic;    //Internal company fund i : 10
+    public float et;    //Employee training
+    public float cf;    //Curriculum for employee training
+}
+
+public class Country : MonoBehaviour
+{
 
     public float A;
-    [Range(50,200)]
-    public float h;
+    [Range(50, 200)]
+    public float Mn;
     public float I;
-    [Range(1,4)]
-    public int SRD = 1;
 
     private const float ipc = 0.001f;
-    private float IO = 0f;
+    private float I0 = 0f;
     private float SP0 = 20f;
     private float MKT0 = 15f;
     private float MAKRET0 = 15f;
@@ -21,15 +120,22 @@ public class Country : MonoBehaviour {
     private float NS0 = 20f;
     private float ST0 = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Interest()
+    {
+        I0 = (SP0 + MKT0 + MAKRET0 + L0 + KH0 + NS0 + ST0);
+        I = A * ipc + I0 * Mn;
+    }
+    // Use this for initialization
+    void Start()
+    {
 
-    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,19 +1,79 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    public DateTime date;
+    public static GameManager Instance;
+    public int SRD = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public DateTime dateGame;
+    public Text txtday1;
+    public Text txtday2;
+    public Text txtmonth1;
+    public Text txtmonth2;
+    public Text txtyear1;
+    public Text txtyear2;
+    public Text txtyear3;
+    public Text txtyear4;
+
+    private float time;
+
+
+    void Awake()
+    {
+        if (Instance != null)
+            return;
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        dateGame = new DateTime(1996, 10, 27);
+        SetDate();
+    }
+
+
+    void SetDate()
+    {
+        if (dateGame.Day >= 10)
+        {
+            txtday1.text = (dateGame.Day / 10).ToString();
+            txtday2.text = (dateGame.Day - (dateGame.Day / 10) * 10).ToString();
+        }
+        else
+        {
+            txtday1.text = "0";
+            txtday2.text = dateGame.Day.ToString();
+        }
+        if (dateGame.Month >= 10)
+        {
+            txtmonth1.text = (dateGame.Month / 10).ToString();
+            txtmonth2.text = (dateGame.Month - (dateGame.Month / 10) * 10).ToString();
+        }
+        else
+        {
+            txtmonth1.text = "0";
+            txtmonth2.text = dateGame.Month.ToString();
+        }
+        string yearstring = dateGame.Year.ToString();
+        txtyear1.text = yearstring.Substring(0, 1);
+        txtyear2.text = yearstring.Substring(1, 1);
+        txtyear3.text = yearstring.Substring(2, 1);
+        txtyear4.text = yearstring.Substring(3, 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 4)
+        {
+            dateGame = dateGame.AddDays(1f);
+            SetDate();
+            Debug.Log(dateGame);
+            time = 0;
+        }
+    }
 }
